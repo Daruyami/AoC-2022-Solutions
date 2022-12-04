d65e8e832befbe877efd2578c285c6d932a9a553
Daruyami <shadedgreen65@gmail.com>
2022-12-04 11:29:33 +0100

    day 4

--- d3/p2.js
+++ d3/p2.js
@@ -1,5 +1,4 @@
 let input = document.getElementsByTagName('pre')[0].textContent.split('\n');
-let chars = "";
 let sumOfValues = 0;
 for(var i = 0; i<input.length-1; i+=3){
 	let len = input[i].length;
@@ -7,18 +6,16 @@ for(var i = 0; i<input.length-1; i+=3){
 	let b = input[i+1].split("");
 	let c = input[i+2].split("");
 	let char = a.filter(
-	valuea => b.filter(
-	valueb => c.includes(valueb)
-	).includes(valuea)
-	)[0];
+					valuea => b.filter(
+						valueb => c.includes(valueb)
+					).includes(valuea)
+				)[0];
 	if(!char)
 		continue;
-	chars += char.toString();
 	if(char.charCodeAt()>96)
 		sumOfValues += char.charCodeAt()-96;
 	else
 		sumOfValues += char.charCodeAt()-38;
 
 }
-console.log(chars);
 console.log(sumOfValues);
---
+++ d4/p1.js
@@ -0,0 +1,15 @@
+function range(start, end) {
+	let size = end-start;
+	return [...Array(size+1).keys()].map(i => i + start);
+}
+let input = document.getElementsByTagName('pre')[0].textContent.split('\n');
+let redundantPairs = 0;
+for(var i = 0; i<input.length-1; i++){
+	let pair = input[i].split(',');
+	let elfa = pair[0].split('-');
+	let elfb = pair[1].split('-');
+	let a = range(parseInt(elfa[0]), parseInt(elfa[1]));
+	let b = range(parseInt(elfb[0]), parseInt(elfb[1]));
+	redundantPairs += a.every(e => b.includes(e)) | b.every(e => a.includes(e));
+}
+console.log(redundantPairs)
